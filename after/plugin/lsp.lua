ec9f5bc441e598dd56615c3a73f752cb00019a20
Lou Hasley <louiemhasley@gmail.com>
2023-06-13 12:59:52 -0500

    accept lsp suggestions

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -12,6 +12,16 @@ lsp.ensure_installed({
 	'rust_analyzer'
 })
 
+local cmp = require('cmp')
+local cmp_select = {behavior = cmp.SelectBehavior.Select}
+local cmp_mappings = lsp.defaults.cmp_mappings({
+  ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
+  ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
+  ['<C-y>'] = cmp.mapping.confirm({ select = true }),
+  ["<C-Space>"] = cmp.mapping.complete(),
+})
+
+
 lsp.on_attach(function(client, bufnr)
 	local opts = {buffer = bufnr, remap = false}
 
@@ -27,4 +37,6 @@ lsp.on_attach(function(client, bufnr)
 	vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
 end)
 
+
 lsp.setup()
+
