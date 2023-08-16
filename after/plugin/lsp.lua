f821123843f2b69d2658f7ca129ae2ad8a1da3f7
Lou Hasley <louiemhasley@gmail.com>
2023-08-16 08:16:29 -0500

    updates

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -13,13 +13,19 @@ lsp.ensure_installed({
 })
 
 local cmp = require('cmp')
-local cmp_select = {behavior = cmp.SelectBehavior.Select}
-local cmp_mappings = lsp.defaults.cmp_mappings({
-  ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
-  ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
-  ['<C-y>'] = cmp.mapping.confirm({ select = true }),
-  ["<C-Space>"] = cmp.mapping.complete(),
+cmp.setup({
+    mapping = {
+        ['<CR>'] = cmp.mapping.confirm({select = false }),
+    }
 })
+-- local cmp_select = {behavior = cmp.SelectBehavior.Select}
+-- local cmp_mappings = lsp.defaults.cmp_mappings({
+--   ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
+--   ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
+--   ['<Tab>'] = cmp.mapping.confirm({ select = true }),
+--   ["<C-Space>"] = cmp.mapping.complete(),
+-- })
+
 
 
 lsp.on_attach(function(client, bufnr)
