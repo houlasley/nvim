8b2cb1f3386f59ec67648bf31f593c3655f9a1f2
houlasley <louiemhasley@gmail.com>
2025-04-21 10:50:11 -0500

    golang

--- lua/config/plugins/lsp.lua
+++ lua/config/plugins/lsp.lua
@@ -1,83 +1,113 @@
 return {
-  {
-    "neovim/nvim-lspconfig",
-    dependencies = {
-      "saghen/blink.cmp",
-      {
-        "folke/lazydev.nvim",
-        ft = "lua", -- only load on lua files
-        opts = {
-          library = {
-            -- See the configuration section for more details
-            -- Load luvit types when the `vim.uv` word is found
-            { path = "${3rd}/luv/library", words = { "vim%.uv" } },
-          },
+  "neovim/nvim-lspconfig",
+  dependencies = {
+    "saghen/blink.cmp",
+    {
+      "folke/lazydev.nvim",
+      ft = "lua",
+      opts = {
+        library = {
+          { path = "${3rd}/luv/library", words = { "vim%.uv" } },
         },
       },
     },
-    config = function()
-      local capabilities = require("blink-cmp").get_lsp_capabilities()
-      local lspconfig = require("lspconfig")
-      local on_attach = function(_, bufnr)
-        local opts = { noremap = true, silent = true, buffer = bufnr }
-        vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
-        vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
-      end
-      lspconfig.lua_ls.setup({
-        capabilities = capabilities,
-        on_attach = on_attach,
-      })
-      lspconfig.pyright.setup({
-        settings = {
-          exclude = { "cdk.out/**" }
-        },
-        capabilities = capabilities,
-        on_attach = on_attach,
-      })
-      lspconfig.ruff.setup({
-        init_options = {
-          settings = {
-            logLevel = 'debug',
-          },
-        },
-        settings = {
-          ruff = {
-            lineLength = 88,
-            select = { "E", "F", "W", "C90", "I" },
-          },
-        },
-        on_attach = on_attach,
-      }
-      )
-
-      vim.keymap.set("n", "<leader>oi", function()
-        vim.lsp.buf.code_action({
-          context = { only = { "source.organizeImports" } },
-          apply = true,
-        })
-      end, { desc = "Organize imports", noremap = true, silent = true })
-
-      vim.api.nvim_create_autocmd("LspAttach", {
-        callback = function(args)
-          local client = vim.lsp.get_client_by_id(args.data.client_id)
-          if not client then
-            return
-          end
-
-          if client.supports_method("textDocument/formatting") then
-            vim.api.nvim_create_autocmd("BufWritePre", {
-              buffer = args.buf,
-              callback = function()
-                vim.lsp.buf.format({ bufnr = args.buf, id = client.id })
-                vim.lsp.buf.code_action({
-                  context = { only = { "source.organizeImports" } },
-                  apply = true,
-                })
-              end,
-            })
-          end
-        end,
-      })
-    end,
   },
+  config = function()
+    local lspconfig = require("lspconfig")
+    local capabilities = require("blink-cmp").get_lsp_capabilities()
+
+    local function on_attach(_, bufnr)
+      local opts = { noremap = true, silent = true, buffer = bufnr }
+      vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+      vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
+    end
+
+    -- Lua
+    lspconfig.lua_ls.setup({
+      capabilities = capabilities,
+      on_attach = on_attach,
+    })
+
+    -- Python
+    lspconfig.pyright.setup({
+      capabilities = capabilities,
+      on_attach = on_attach,
+    })
+
+    -- Golang
+    lspconfig.gopls.setup({
+      capabilities = capabilities,
+      on_attach = on_attach,
+      settings = {
+        gopls = {
+          gofumpt = true,
+          staticcheck = true,
+          usePlaceholders = true,
+          analyses = {
+            unusedparams = true,
+            shadow = true,
+          },
+        },
+      },
+    })
+    -- Ruff
+    lspconfig.ruff.setup({
+      capabilities = capabilities,
+      on_attach = on_attach,
+      settings = {
+        ruff = {
+          lineLength = 88,
+          select = { "E", "F", "W", "C90", "I" },
+        },
+      },
+    })
+
+    -- TypeScript + Vue
+    lspconfig.ts_ls.setup({
+      capabilities = capabilities,
+      on_attach = function(client, bufnr)
+        on_attach(client, bufnr)
+
+        -- Format on save
+        if client.supports_method("textDocument/formatting") then
+          vim.api.nvim_create_autocmd("BufWritePre", {
+            buffer = bufnr,
+            callback = function()
+              vim.lsp.buf.format({ bufnr = bufnr })
+            end,
+          })
+        end
+      end,
+      init_options = {
+        plugins = {
+          {
+            name = "@vue/typescript-plugin",
+            location = vim.fn.getcwd() .. "/node_modules/@vue/typescript-plugin",
+            languages = { "javascript", "typescript", "vue" },
+          },
+        },
+      },
+      filetypes = { "typescript", "javascript", "vue" },
+      root_dir = lspconfig.util.root_pattern("package.json", "tsconfig.json", ".git"),
+    })
+
+    -- Auto format + organize imports on save
+    vim.api.nvim_create_autocmd("LspAttach", {
+      callback = function(args)
+        local client = vim.lsp.get_client_by_id(args.data.client_id)
+        if client and client.supports_method("textDocument/formatting") then
+          vim.api.nvim_create_autocmd("BufWritePre", {
+            buffer = args.buf,
+            callback = function()
+              vim.lsp.buf.format({ bufnr = args.buf })
+              vim.lsp.buf.code_action({
+                context = { only = { "source.organizeImports" } },
+                apply = true,
+              })
+            end,
+          })
+        end
+      end,
+    })
+  end,
 }
